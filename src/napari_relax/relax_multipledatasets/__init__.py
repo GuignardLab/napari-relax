37bb10cbf234e3fd921fd158902f1e55d3e44f3e
BadPrograms <johnliaskas@gmail.com>
2025-07-24 11:31:42 +0300

    Deployed 7df4050 with MkDocs version: 1.6.1

--- src/napari_relax/relax_multipledatasets/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cross_embryo_comparison import Embryo_comparisons
-from .manager_widget import cross_embryo
-
-__all__ = ("cross_embryo", "Embryo_comparisons")
-
-# All new widget should be listed here to be displayed in napari
-__all_widgets__ = (cross_embryo, Embryo_comparisons)
-__overall_widget__ = ()
