3909fae2e65b720fefa449d3613978847fcb10d0
BadPrograms <johnliaskas@gmail.com>
2025-08-04 12:10:59 +0300

    fix circular import

--- src/napari_relax/_util_classes/__init__.py
+++ src/napari_relax/_util_classes/__init__.py
@@ -7,21 +7,20 @@ from .dialog_for_reader import (
 from .eventfilter_for_delayed_tooltip import (
     delayedtooltipeventfilter,
 )
+from .tab_template import tab_template
 from .layer_corrector import Layer_corrector_Tree_Producer
-from ..relax_multipledatasets.tab_template import tab_template
+
 from .tooltip import tooltip_button
 from .viewer_wrapper import QtViewerWrap
 
 __all__ = (
     "containerize",
     "Layer_corrector_Tree_Producer",
-    "tab_template",
     "QtViewerWrap",
-    "single_tree",
     "loading_dialog",
     "time_res_dialog",
+    "tab_template",
     "delayedtooltipeventfilter",
     "tooltip_button",
     "big_dataset_names_dialog",
-    "single_tree_progeny",
 )
