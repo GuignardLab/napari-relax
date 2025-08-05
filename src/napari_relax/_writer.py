ed21ccc900fed0286313fbcd7fe9060587f3838a
BadPrograms <johnliaskas@gmail.com>
2025-08-05 17:54:57 +0300

    fix!

--- src/napari_relax/_writer.py
+++ src/napari_relax/_writer.py
@@ -1,13 +1,13 @@
 from lineagetree import LineageTree
 from napari.layers import Points
+from typing import TYPE_CHECKING, Any, Union
+
 import os
 
 
-def napari_get_writer(path: str, layer_type: Points):
-    if "LineageTree" in layer_type.metadata:
+def write_single_image(path: str, data: Any, meta: dict) -> list[str]:
+    if "LineageTree" in meta["metadata"]:
         if not path.endswith(("lT", "lt", "LT")):
             path = os.path.splitext(path)[0] + ".lT"
-        layer_type.metadata["LineageTree"].write(path)
-        return path
-    else:
-        return None
+        meta["metadata"]["LineageTree"].write(path)
+    return [path]
