d22981e33c9ba7feb1b28b89ecab3a1d282fbfa4
Léo Guignard <leo.guignard@univ-amu.fr>
2025-08-28 10:21:56 +0200

    adding the c. elegans dataset

--- src/napari_relax/demo_data/__init__.py
+++ src/napari_relax/demo_data/__init__.py
@@ -1,5 +1,6 @@
-from ._load_demo import load_demo
+from ._load_demo import load_c_elegans, load_demo
 
 __all__ = [
     "load_demo",
+    "load_c_elegans",
 ]
--- src/napari_relax/demo_data/_load_demo.py
+++ src/napari_relax/demo_data/_load_demo.py
@@ -13,3 +13,13 @@ def load_demo():
     demo_data.time_resolution = 10
     data = layer_preparation(demo_data, "Demo")
     return data
+
+
+def load_c_elegans():
+    """Adds the lineatree to the viewer"""
+    demo_data = LineageTree.load(
+        os.path.join(directory, "c.elegans.smoothed.lT")
+    )
+    demo_data.time_resolution = 1
+    data = layer_preparation(demo_data, "C.elegans")
+    return data
