a4e2983561db829482fd3b17c3dd83bec248c174
jules-vanaret <jules.vanaret@univ-amu.fr>
2025-08-29 12:09:35 +0200

    updated init

--- src/napari_relax/demo_data/__init__.py
+++ src/napari_relax/demo_data/__init__.py
@@ -1,5 +1,16 @@
-from ._load_demo import load_demo
+from ._load_demo import load_demo, load_c_elegans
+from ._download_utils import (
+    ensure_demo_data,
+    get_cached_datasets,
+    clear_cache,
+)
+from ._datasets import DEMO_DATASETS
 
 __all__ = [
     "load_demo",
+    "load_c_elegans",
+    "ensure_demo_data", 
+    "get_cached_datasets",
+    "clear_cache",
+    "DEMO_DATASETS"
 ]
