5916dcd5ae7ae70cce59ff64584c64fbeb2c5e01
Léo Guignard <leo.guignard@univ-amu.fr>
2025-09-02 15:02:42 +0200

    Quick re-ordering of imports

--- src/napari_relax/demo_data/__init__.py
+++ src/napari_relax/demo_data/__init__.py
@@ -1,15 +1,15 @@
-from ._load_demo import load_demo
+from ._datasets import DEMO_DATASETS
 from ._download_utils import (
+    clear_cache,
     ensure_demo_data,
     get_cached_datasets,
-    clear_cache,
 )
-from ._datasets import DEMO_DATASETS
+from ._load_demo import load_demo
 
 __all__ = [
     "load_demo",
-    "ensure_demo_data", 
+    "ensure_demo_data",
     "get_cached_datasets",
     "clear_cache",
-    "DEMO_DATASETS"
+    "DEMO_DATASETS",
 ]
