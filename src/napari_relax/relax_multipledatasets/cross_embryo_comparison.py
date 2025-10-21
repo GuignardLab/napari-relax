bbe9735ed9ad796e8ab90c398d9d3c904709ea29
BadPrograms <johnliaskas@gmail.com>
2025-10-21 13:41:09 +0200

    fix the layout of the clustermaps

--- src/napari_relax/relax_multipledatasets/cross_embryo_comparison.py
+++ src/napari_relax/relax_multipledatasets/cross_embryo_comparison.py
@@ -193,6 +193,7 @@ class CrossClustermap(LayerCorrectorTreeProducer):
 
     def _click(self, event):
         if event.button == 1:
+            self.canvas.figure.set_constrained_layout(False)
             layers = [
                 self.labels_lT[int(event.xdata + 0.5)],
                 self.labels_lT[int(event.ydata + 0.5)],
@@ -227,7 +228,7 @@ class CrossClustermap(LayerCorrectorTreeProducer):
     def clustermap_creator(self):
         plt.close("all")
         time = int(self.time_slider.value)
-
+        self.canvas.figure.set_constrained_layout(True)
         self.range = len(self.comparisons)
 
         len_all_trees = len(self.names[time].keys())
