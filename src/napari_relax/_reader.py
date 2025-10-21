93bc132bf36d37d5ffa432af0b1ec99181dc4f01
BadPrograms <johnliaskas@gmail.com>
2025-10-21 14:45:29 +0200

    change in lineagetree :)

--- src/napari_relax/_reader.py
+++ src/napari_relax/_reader.py
@@ -16,7 +16,7 @@ from lineagetree import (
     read_from_mastodon,
     read_from_tgmm_xml,
 )
-from lineagetree._basics import utils
+from lineagetree._core import utils
 from napari.utils import colormaps
 
 from ._util_classes import loading_dialog, time_res_dialog
